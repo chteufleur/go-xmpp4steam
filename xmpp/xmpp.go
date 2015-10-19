bc9eee01f3b4a116566c8fcef062b511bdcd9cde
Chteufleur <chteufleur@kingpenguin.tk>
2015-10-19 21:42:01 +0200

    go fmt

--- xmpp/xmpp.go
+++ xmpp/xmpp.go
@@ -1,95 +1,93 @@
 package xmpp
 
-
 import (
-  // "github.com/emgee/go-xmpp"
-  "go-xmpp"
+	// "github.com/emgee/go-xmpp"
+	"go-xmpp"
 
-  "log"
-  "strings"
+	"log"
+	"strings"
 )
 
 const (
-  Status_online = ""
-  Status_offline = ""
-  Status_away = "away"
-  Status_chat = "chat"
-  Status_do_not_disturb = "dnd"
-  Status_extended_away = "xa"
+	Status_online         = ""
+	Status_offline        = ""
+	Status_away           = "away"
+	Status_chat           = "chat"
+	Status_do_not_disturb = "dnd"
+	Status_extended_away  = "xa"
 
-  Type_available = ""
-  Type_unavailable = "unavailable"
+	Type_available   = ""
+	Type_unavailable = "unavailable"
 
-  ActionConnexion = "action_xmpp_connexion"
-  ActionDeconnexion = "action_xmpp_deconnexion"
+	ActionConnexion   = "action_xmpp_connexion"
+	ActionDeconnexion = "action_xmpp_deconnexion"
 
-  LogInfo = "\t[XMPP INFO]\t"
-  LogError = "\t[XMPP ERROR]\t"
-  LogDebug = "\t[XMPP DEBUG]\t"
+	LogInfo  = "\t[XMPP INFO]\t"
+	LogError = "\t[XMPP ERROR]\t"
+	LogDebug = "\t[XMPP DEBUG]\t"
 )
 
 var (
 	Addr   = "127.0.0.1:5347"
-  JidStr = ""
+	JidStr = ""
 	Secret = ""
 
-  PreferedJID = ""
+	PreferedJID = ""
 
-  jid xmpp.JID
-  stream = new(xmpp.Stream)
-  comp   = new(xmpp.XMPP)
+	jid    xmpp.JID
+	stream = new(xmpp.Stream)
+	comp   = new(xmpp.XMPP)
 
-  ChanPresence = make(chan string)
-  ChanMessage = make(chan string)
-  ChanAction = make(chan string)
+	ChanPresence = make(chan string)
+	ChanMessage  = make(chan string)
+	ChanAction   = make(chan string)
 
-  CurrentStatus = Status_offline
+	CurrentStatus = Status_offline
 
-  Version = ""
+	Version = ""
 )
 
-
 func Run() {
-  log.Printf("%sRunning", LogInfo)
+	log.Printf("%sRunning", LogInfo)
 	// Create stream and configure it as a component connection.
 	jid = must(xmpp.ParseJID(JidStr)).(xmpp.JID)
 	stream = must(xmpp.NewStream(Addr, &xmpp.StreamConfig{LogStanzas: true})).(*xmpp.Stream)
 	comp = must(xmpp.NewComponentXMPP(stream, jid, Secret)).(*xmpp.XMPP)
 
-  SendPresence(Status_online, Type_available)
+	SendPresence(Status_online, Type_available)
 
-  mainXMPP()
+	mainXMPP()
 }
 
 func mainXMPP() {
 	for x := range comp.In {
-    switch v := x.(type) {
-    case *xmpp.Presence:
-      if strings.SplitN(v.From, "/", 2)[0] == PreferedJID && v.To == JidStr {
-        if v.Type == Type_unavailable {
-          Disconnect()
-          ChanAction <- ActionDeconnexion
-        } else {
-          // SendPresence(v.Show, v.Type)
-          CurrentStatus = v.Show
-          ChanAction <- ActionConnexion
-        }
+		switch v := x.(type) {
+		case *xmpp.Presence:
+			if strings.SplitN(v.From, "/", 2)[0] == PreferedJID && v.To == JidStr {
+				if v.Type == Type_unavailable {
+					Disconnect()
+					ChanAction <- ActionDeconnexion
+				} else {
+					// SendPresence(v.Show, v.Type)
+					CurrentStatus = v.Show
+					ChanAction <- ActionConnexion
+				}
 
-        ChanPresence <- v.Show
-      }
+				ChanPresence <- v.Show
+			}
 
-    case *xmpp.Message:
-      steamID := strings.SplitN(v.To, "@", 2)[0]
-      ChanMessage <- steamID
-      ChanMessage <- v.Body
+		case *xmpp.Message:
+			steamID := strings.SplitN(v.To, "@", 2)[0]
+			ChanMessage <- steamID
+			ChanMessage <- v.Body
 
-    default:
-	    log.Printf("%srecv: %v", LogDebug, x)
-    }
+		default:
+			log.Printf("%srecv: %v", LogDebug, x)
+		}
 	}
 
-  // Send deconnexion
-  SendPresence(Status_offline, Type_unavailable)
+	// Send deconnexion
+	SendPresence(Status_offline, Type_unavailable)
 }
 
 func must(v interface{}, err error) interface{} {
@@ -100,17 +98,17 @@ func must(v interface{}, err error) interface{} {
 }
 
 func Disconnect() {
-  SendPresence(Status_offline, Type_unavailable)
+	SendPresence(Status_offline, Type_unavailable)
 }
 
 func SendPresence(status, tpye string) {
-  comp.Out <- xmpp.Presence{To: PreferedJID, From: jid.Domain, Show: status, Type: tpye, Status: "go-xmpp4steam v"+Version}
+	comp.Out <- xmpp.Presence{To: PreferedJID, From: jid.Domain, Show: status, Type: tpye, Status: "go-xmpp4steam v" + Version}
 }
 
 func SendPresenceFrom(status, tpye, from string) {
-  comp.Out <- xmpp.Presence{To: PreferedJID, From: from, Show: status, Type: tpye}
+	comp.Out <- xmpp.Presence{To: PreferedJID, From: from, Show: status, Type: tpye}
 }
 
 func SendMessage(from, message string) {
-  comp.Out <- xmpp.Message{To: PreferedJID, From: from, Body: message}
+	comp.Out <- xmpp.Message{To: PreferedJID, From: from, Body: message}
 }
